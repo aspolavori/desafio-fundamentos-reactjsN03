bb5d8e9bb7a9466bc7c45b2120171ec10b9c207f
Anderson Spolavori <aspolavori@yahoo.com.br>
2021-02-16 23:31:21 +0000

    .

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
-
 import { Container } from './styles';
 
 import Logo from '../../assets/logo.svg';
-
 interface HeaderProps {
   size?: 'small' | 'large';
 }
@@ -14,10 +11,13 @@ const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
   <Container size={size}>
     <header>
       <img src={Logo} alt="GoFinances" />
-      <nav>
-        {
-          // Todo
-        }
+      <nav>        
+          <Link key="id" to="/">
+            Listagem
+          </Link>
+          <Link key="id" to="/import" >
+            Importar
+          </Link>        
       </nav>
     </header>
   </Container>
